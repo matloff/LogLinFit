5110630f5f7cd5553864e6f0107b3227ff2a008b
matloff <matloff@cs.ucdavis.edu>
2019-08-27 20:29:29 -0700

    approaching a package

--- R/simp_func.R
+++ R/simp_func.R
@@ -1,9 +1,5 @@
 library(regtools)
 
-## Example data
-ucb <- UCBAdmissions
-ucbdf <- as.data.frame(ucb)
-
 ################ new_data() ################
 
 ## inputs original dataframe
@@ -18,70 +14,66 @@ new_data <- function(data)
 }
 
 ## Newdata = original data + count of each combination
-newdata <- new_data(newd)
+########### newdata <- new_data(newd)
+newdata <- new_data(ucbdf)
 
 ################ cat_pred_auto() ################
 
-## inputs new data
+# arguments
 
-## inputs degree of polynomial
+#   data: either an R table or a data frame; in latter case, last column is
+#      frequencies
+#   degree: maximum interaction degree, e.g. 2 for interactions 
+#      between pairs of variables
 
-## input data type, defaul type="df"
-
-cat_pred_auto <- function(data, degree, type = "df")
+cat_pred_auto <- function(data, degree)
 {
-   if(type == "tb")
-      {
-      df <- as.data.frame(data)	
-      }
-   else
-      {
-      df <- data	
-      }
-   ncol <- ncol(df) - 1
+   if(class(data) == "table") dFrame <- as.data.frame(data)	
+   else dFrame <- data	
+      
+   ncol <- ncol(dFrame) - 1  
    if(degree > ncol)
-      {
-      stop("The degree of combination is greater than the number of variables", call. = FALSE)
-      }
-   else if(degree == 1)
-      {
-      var_x <- colnames(df)[1:ncol]
-      var_y <- colnames(df)[ncol + 1]
+      stop("degree greater than number of variables", call. = FALSE)
+   var_x <- colnames(dFrame)[1:ncol]
+   var_y <- colnames(dFrame)[ncol + 1]
+   if(degree == 1) {
       rh_formula <- paste(var_x, collapse = " + ")
-      formula_string <- paste(var_y, rh_formula, sep = " ~ ")
-      formula <- as.formula(formula_string)
-      est <- glm(formula, data=df, family="poisson")
+      est <- fitGLM(rh_formula,dFrame,var_y)
       betas <- est$coef
       variance <- diag(vcov(est))
       beta_var <- list("beta" = betas, "var" = variance)
       return(beta_var)
-      }
-   else
-      {
-      var_x <- colnames(df)[1:ncol]
-      var_y <- colnames(df)[ncol + 1]
+   } else {
       formula_list = list()
       for(i in 1:degree)
          {
-         product <- combn(colnames(df)[1:ncol], i,
+         product <- combn(colnames(dFrame)[1:ncol], i,
                     FUN = paste0, collapse = "*" ,  simplify = FALSE)
          sum_pro <- paste(product, collapse = " + ")
          formula_list[[i]] <- sum_pro
          }
       rh_formula <- paste(formula_list, collapse = " + ")
-      formula_string <- paste(var_y, rh_formula, sep = " ~ ")
-      formula <- as.formula(formula_string)
-      est <- glm(formula, data=df, family="poisson")
+      est <- fitGLM(rh_formula,dFrame,var_y)
       betas <- est$coef
-      variance <- diag(vcov(est))
-      beta_var <- list("beta" = betas, "var" = variance)
+      se <- sqrt(diag(vcov(est)))
+      beta_var <- list("beta" = betas, "se" = se)
       return(beta_var)
-      }
+   }
 }
 
-## Example code
-test1 <- cat_pred_auto(ucb, 2, type="tb"); test1
-test2 <- cat_pred_auto(ucbdf, 2); test2
+fitGLM <- function(rh_formula,dFrame,var_y) {
+      formula_string <- paste(var_y, rh_formula, sep = " ~ ")
+      formula <- as.formula(formula_string)
+      glm(formula, data=dFrame, family=poisson)
+}
+
+
+
+## Example 
+### ucb <- UCBAdmissions
+### ucbdf <- as.data.frame(ucb)
+### test1 <- cat_pred_auto(ucb, 2, type="tb"); summary(test1)
+### test2 <- cat_pred_auto(ucbdf, 2); test2
 
 ################ cat_pred_self() ################
 
@@ -89,7 +81,7 @@ test2 <- cat_pred_auto(ucbdf, 2); test2
 
 ## inputs function of polynomial as string
 
-## input data type, defaul type="df"
+## input data type, default type="df"
 
 cat_pred_self <- function(data, rh_formula, type = "df")
 {
@@ -113,6 +105,6 @@ cat_pred_self <- function(data, rh_formula, type = "df")
 }
 
 ## Example code
-form <- "persfin + natecon + persfin*natecon"
-result2 <- cat_pred_self(newdata, form)
+### form <- "persfin + natecon + persfin*natecon"
+### result2 <- cat_pred_self(newdata, form)
 
